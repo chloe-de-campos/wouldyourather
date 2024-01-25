b9301c08774df0d98a387cc772421cbf55b4e805
chloe de campos <90464499+chloe-de-campos@users.noreply.github.com>
2024-01-24 23:20:43 -0500

    woohoo!

--- src/project-pages/NuGo/NuGo.js
+++ src/project-pages/NuGo/NuGo.js
@@ -1,6 +1,8 @@
 import "../../App.css"
 import { useNavigate } from "react-router-dom";
 
+
+
 const NuGo = ()  => {
     
     let navigate = useNavigate(); 
@@ -13,45 +15,40 @@ const NuGo = ()  => {
         <div>ls
             <div class="topBuffer"> </div>
             <div class="projectDescription">
+            <img src={require('./nugo mockup dark short.png')}></img>
             <p className="date">January 2024</p>
                 <h2>Nugo</h2>
 
-                <p>I found these bars </p>
+                <p>I'm an athlete and a vegetarian and a college student. At the center of that venn diagram you find a person who is constantly scavenging for protein. I almost didn't buy a NuGo bar because the packaging was so offputting--and I'm vain about this stuff--but I liked the ingredients so I took a risk. The risk paid off, and I found myself not only satisfied but hooked, purchasing bars in every available flavor the following week. I started a redesign journey with the goal of creating packaging that would not only attract but excite me as a consumer.</p>
 
-                <h3>Challenge</h3>
 
+                <h3>Challenges</h3>
+
+                <img src={require('./old_bar.png')}></img>
                 <p>
                 The original packaging presented several challenges:
 
                     <ol>
-                        <li><emph> Weird Logo:</emph> I found that the rotation of the box and font of the original logo disrupted the visual harmony. This issue led to a lack of cohesiveness and a less professional aesthetic. </li>
+                        <li><b> Weird Logo:</b> I found that the rotation of the box and font of the original logo disrupted the visual harmony. This issue led to a lack of cohesiveness and a less professional aesthetic. </li>
 
-                        <li><emph> Repeated Information: </emph>Repetition of information on the packaging caused confusion and clutter. Streamlining the content was crucial for a cleaner and more effective design.</li>
+                        <li><b> Repeated Information: </b>Repetition of information on the packaging caused confusion and clutter. Streamlining the content was crucial for a cleaner and more effective design.</li>
 
-                        <li><emph>Typographic Confusion:</emph> The typography lacked clear hierarchy, making it difficult for consumers to grasp essential information quickly. Clarity in communication was a key area for improvement.</li>
+                        <li><b>Typographic Confusion:</b> The typography lacked clear hierarchy, making it difficult for consumers to grasp essential information quickly. Clarity in communication was a key area for improvement.</li>
 
-                        <li><emph>Unclear Audience:</emph> The existing design had a confusing blend of sports nutrition and delicate chocolatey decadence, making it challenging to identify the target audience.</li>
-
-                        
+                        <li><b>Unclear Audience:</b> I couldn't tell who the design was for. It had a confusing blend of sportsy, proteiny vibes, delicate chocolatey decadence, and </li>
                     </ol>
                 </p>
 
-                <h3>Objectives</h3>
-
+                <h3>Redesign Solutions</h3>
+                <img src={require('./new_bar.png')}></img>
                 <p>
                     <ol>
-                        <li><emph>Enhance Visual Appeal:</emph> Create a packaging design that catches the consumer's eye and communicates the energy bar's quality. </li>
-                        <li><emph> Brand Consistency: </emph>Ensure the new design aligns with the overall brand image and values.</li>
-                        <li><emph>Informative Design: </emph>Incorporate key information about the product, benefits, and usage to guide the consumer.</li>
+                        <li><b>Connect To Target Market:</b> Adopted an earthy, natural style to resonate with the target market, including vegans and those with specific dietary concerns, creating a visually cohesive and appealing packaging.</li>       
+                        <li><b>Enhance Visual Appeal:</b> Created a packaging design that catches the consumer's eye and communicates the energy bar's quality. Incorporated organic illustrations showcasing ingredients and flavors, fostering a connection with health-conscious consumers seeking natural products.</li>
+                        <li><b>Real Dark Chocolate Focus: </b>Emphasized the phrase "Real Dark Chocolate" by using a decadent dripping blobby shape and a dark chocolatey color. This approach maintains prominence without overwhelming the packaging with large text.</li>
                     </ol>
                 </p>
-
-                <p>Impractical problems require impractical solutions. Mars is an app designed for students to communicate during class in the most obnoxious way possible to their peers. It uses javascript and React to translate messages from english to morsecode and then strobes the webpage to transmit it to those in view. It also includes a decoding interface for recipients to record and translate messages back to english.</p>
-
-                <p>This stupid app is named for my brilliant friend Mars, who used to sit behind me and whose attention I really really wanted. </p>
-            
-                <p><a target="_blank" href="https://chloe-de-campos.github.io/mars/">Send a message to mars</a>.</p> 
-
+   
                 <button className="button" onClick={() => navigate(-1)} type="button">
                 Return to Projects</button>
             </div> 
