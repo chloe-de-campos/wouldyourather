a4869dae00cdb3437b15c9539a205f2938180761
chloe de campos <chloe_de_campos@brown.edu>
2024-06-07 13:53:29 -0400

    firebase

--- src/Home.js
+++ src/Home.js
@@ -5,27 +5,7 @@ import { useState, useEffect} from "react";
 
 import WorkGallery from "./components/WorkGallery";
 
-// Home page! displays gallery of my work.
 
-// const Home = () =>{
-//     return (
-//         <div className="mainApp"> 
-            
-//             <div className="AppCenter">
-//                 <div className="topBuffer">
-//                 </div>
-
-//                 <div id="header">
-//                 </div>
-                
-//                 {WorkGallery()}
-//             </div>
-    
-//     </div>)
-
-//   }
-  
-//   export default <Home/>;
 
 function Chloe() { 
 
